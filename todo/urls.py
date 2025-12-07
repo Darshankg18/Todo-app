96543f4c493be23c1ddfc81cc4d422c9a88d3352
Darshan K G <kgdarshan2005@gmail.com>
2025-12-07 19:36:26 +0530

    Updated

--- todo/urls.py
+++ todo/urls.py
@@ -2,4 +2,12 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('addTask/',views.addTask,name='addTask'),
+    #Mark as Undone
+    path('mark_as_done/<int:pk>/',views.mark_as_done,name='mark_as_done'),
+    #Mark as undone
+    path('mark_as_undone/<int:pk>/',views.mark_as_undone,name='mark_as_undone'),
+    #Edit
+    path('edit_task/<int:pk>/',views.edit_task,name='edit_task'),
+    #Delete
+    path('delete_task/<int:pk>/',views.delete_task,name='delete_task')
 ]
